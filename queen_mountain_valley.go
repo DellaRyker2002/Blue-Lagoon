3498d97392e772199c0eab70f0433d1558b5b230
DellaRyker2002 <137502362+DellaRyker2002@users.noreply.github.com>
2023-06-25 08:03:22 +0700

    Simplify code logic

---
+++ queen_mountain_valley.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Welcome to the Wellness Center!")
+	
+	fmt.Println("We offer an array of holistic health services to help you find balance and relaxation in your life.")
+	
+	fmt.Println("We offer individual and group services, including:")
+	
+	fmt.Println("- Massage therapy")
+	fmt.Println("- Acupuncture")
+	fmt.Println("- Reiki")
+	fmt.Println("- Nutritional counseling")
+	fmt.Println("- Aromatherapy")
+	fmt.Println("- Herbal remedies")
+	fmt.Println("- Yoga")
+	fmt.Println("- Meditation")
+	
+	fmt.Println("We offer many packages designed to help you find the balance you are seeking in your life. We also offer private sessions and customized programs to suit your individual needs.")
+	
+	fmt.Println("We provide a place of healing and relaxation, taking a holistic approach to health and wellness. Our mission is to provide quality, compassionate care, to help you reach your maximum potential for optimal health.")
+	
+	fmt.Println("We welcome you to come in and experience the peace and serenity of the Wellness Center. Let us help you find the balance you are looking for in life! ")
+}
